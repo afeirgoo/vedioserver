432a576fcca51683b48a30c5586568f6777e3726
Administrator <Administrator@BMPE4PQ6PRU3JOB>
2016-06-28 23:40:45 +0800

    修改了java接收包时字节流高地位处理不当的问题

--- src/com/xjtu/multithread/ServiceImpl.java
+++ src/com/xjtu/multithread/ServiceImpl.java
@@ -7,7 +7,8 @@ import java.nio.ByteBuffer;
 import java.sql.Timestamp;
 import java.util.Arrays;
 import java.util.Date;
-
+import java.nio.ByteBuffer; 
+import java.nio.ByteOrder; 
 /**
  * @说明 打印收到的数据包，并且将数据原封返回，中间设置休眠表示执行耗时
  * @author afeirgoo
@@ -33,7 +34,7 @@ class ServiceImpl implements Runnable {
 			//Thread.sleep(1 * 1000); // 5秒才返回，标识服务端在处理数据
 			// 设置回复的数据，原数据返回，以便客户端知道是那个客户端发送的数据
 			packet.setData(bt);
-			//UdpService.response(packet);
+			UdpService.response(packet);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -60,8 +61,14 @@ class ServiceImpl implements Runnable {
     	}
     	byte[] btTemp = new byte[2];
     	btTemp = new byte[2];
-	    System.arraycopy(pt, 0, btTemp, 0, 2);  //取出4个字节
-	    int len = StreamTool.byteToShort(btTemp);  //数据包总长度    	
+	    System.arraycopy(pt, 0, btTemp, 0, 2);  //取出2个字节	
+	    ByteBuffer buffer =  ByteBuffer.wrap(btTemp); 
+	    //System.out.println(buffer.getShort()); 
+	    int len = buffer.getShort();
+	    //int len = StreamTool.byteToShort(btTemp);  //数据包总长度    
+	    byte[] str_MM;
+	    byte[] str_STA;
+	    byte flag;
     	if(len != pt.length)
     	{
     		return ret;
@@ -75,15 +82,17 @@ class ServiceImpl implements Runnable {
 	    		ret = 1;
 	    		btTemp = new byte[4];
 	    	    System.arraycopy(pt, 5, btTemp, 0, 4);  //取出4个字节
-	    	    deviceID = StreamTool.bytesToInt(btTemp);  //设备ID
-	    	    byte[] str_MM = new byte[16];
+	    	    buffer =  ByteBuffer.wrap(btTemp); 
+	    	    deviceID = buffer.getInt();
+	    	    //deviceID = StreamTool.bytesToInt(btTemp);  //设备ID
+	    	    str_MM = new byte[16];
 	    	    System.arraycopy(pt, 9, str_MM, 0, 16);  //取出16个字节
-	    	    byte[] str_STA = new byte[len - 26];
+	    	    str_STA = new byte[len - 26];
 	    	    System.arraycopy(pt, 25, str_STA, 0, (len - 26));  //取出(len - 26)个字节
-	    	    byte flag = pt[len - 1]; //标志位是最后一字节
-	    	    System.out.println(len);	
-	    	    System.out.println(str_MM.toString());	
-	    	    System.out.println(str_STA.toString());	
+	    	    flag = pt[len - 1]; //标志位是最后一字节
+	    	    //System.out.println(len);	
+	    	    //System.out.println(str_MM.toString());	
+	    	    //System.out.println(str_STA.toString());	
 	    	    //构造返回包
 	    	    ByteBuffer bf = ByteBuffer.allocate(ackheartpaketheadlen);
 	    	    short temp = ackheartpaketheadlen;
@@ -102,7 +111,35 @@ class ServiceImpl implements Runnable {
 	    		break;
 	    	case 9:
 	    		//这是设备执行命令之后给服务器的返回信息,用于控制者知道设备执行情况
-	    		ret = 9;
+	    		ret = 9;	    		
+	    		btTemp = new byte[4];
+	    	    System.arraycopy(pt, 5, btTemp, 0, 4);  //取出4个字节
+	    	    buffer =  ByteBuffer.wrap(btTemp); 
+	    	    int userID = buffer.getInt();
+	    	    btTemp = new byte[4];
+	    	    System.arraycopy(pt, 9, btTemp, 0, 4);  //取出4个字节
+	    	    buffer =  ByteBuffer.wrap(btTemp); 
+	    	    int ID = buffer.getInt();
+	    	    str_MM = new byte[16];
+	    	    System.arraycopy(pt, 13, str_MM, 0, 16);  //取出16个字节
+	    	    str_STA = new byte[len - 30];
+	    	    System.arraycopy(pt, 29, str_STA, 0, (len - 30));  //取出(len - 26)个字节
+	    	    flag = pt[len - 1]; //标志位是最后一字节
+	    	    
+	    	    ByteBuffer ackbf = ByteBuffer.allocate(7);
+	    	    short templen = 7;
+	    	    ackbf.put(StreamTool.shortToByte(templen));    // 总长度   //可能还需要确认，修改
+	    	    ackbf.put(versionid);    //版本号
+	    	    ackbf.put(devicetype);    //设备类型
+	    	    ackbf.put((byte) 9);    //9对应16进制是9H，表示该包是设备执行结果包的确认包
+	    	    ackbf.put((byte) 1);    //确认结果 	       
+	    	    
+	    	    ackbf.put((byte) 5);     //成功   	       	    
+	    	    //这里实际上要给发送方回复, 以便客户端继续发送
+	    	    packet.setData(ackbf.array());
+	    	    UdpService.response(packet);  		
+	    		
+	    		
 	    		break;
 	    	case 10:
 	    		//这是发图片,要提取图片数据，等待完全收完后重组图片
@@ -110,21 +147,32 @@ class ServiceImpl implements Runnable {
 		    		ret = 10;
 		    		btTemp = new byte[4];
 		    	    System.arraycopy(pt, 5, btTemp, 0, 4);  //取出4个字节
-		    	    deviceID = StreamTool.bytesToInt(btTemp);  //设备ID
+		    	    buffer =  ByteBuffer.wrap(btTemp); 
+		    	    deviceID = buffer.getInt();
+		    	    //deviceID = StreamTool.bytesToInt(btTemp);  //设备ID
 		    	    byte[] smallbtTemp = new byte[2];
 		    	    System.arraycopy(pt, 9, smallbtTemp, 0, 2);  //取出2个字节
-		    	    short totalpacketnum = StreamTool.byteToShort(smallbtTemp);
+		    	    buffer =  ByteBuffer.wrap(smallbtTemp); 
+		    	    
+		    	    //short totalpacketnum = StreamTool.byteToShort(smallbtTemp);
+		    	    short totalpacketnum = buffer.getShort();
 		    	    smallbtTemp = new byte[2];
 		    	    System.arraycopy(pt, 11, smallbtTemp, 0, 2);  //取出2个字节
-		    	    short currentpacketnum = StreamTool.byteToShort(smallbtTemp);   //当前包序号
+		    	    buffer =  ByteBuffer.wrap(smallbtTemp);
+		    	    //short currentpacketnum = StreamTool.byteToShort(smallbtTemp);   //当前包序号
+		    	    short currentpacketnum = buffer.getShort();
 		    	    System.out.println("Server rev " + currentpacketnum);
 		    	    btTemp = new byte[4];
 		    	    System.arraycopy(pt, 13, btTemp, 0, 4);   //发送的图片名，不能太长，浪费资源
-		    	    Integer name = StreamTool.bytesToInt(btTemp);
+		    	    buffer =  ByteBuffer.wrap(btTemp);
+		    	    //Integer name = StreamTool.bytesToInt(btTemp);
+		    	    Integer name = buffer.getInt();
 		    	    String nameStr = name.toString();		    	    
 		    	    btTemp = new byte[4];
 		    	    System.arraycopy(pt, 17, btTemp, 0, 2);  //取出2个字节
-		    	    short datalength = StreamTool.byteToShort(btTemp);   //纯图片数据的长度
+		    	    //short datalength = StreamTool.byteToShort(btTemp);   //纯图片数据的长度
+		    	    buffer =  ByteBuffer.wrap(btTemp);
+		    	    short datalength = buffer.getShort();
 		    	    filePath += String.valueOf(deviceID); //按照设备ID将图片分别存放
 		    	    filePath += "\\";
 		    	    //如果设备ID的文件夹不存在就创建
